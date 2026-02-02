179db645469cd500fa58c8ad3c60b5288088ccfd
hsxxxx <hsxxxx@gmail.com>
2026-02-03 05:45:14 +0900

    실행오류

--- apps/ecosystem.config.js
+++ apps/ecosystem.config.js
@@ -3,26 +3,22 @@ module.exports = {
     {
       name: "bybit-collector",
       cwd: __dirname + "/apps/collector",
-      script: "node",
-      args: ["dist/index.js"],
+      script: "dist/index.js",
+      interpreter: "/home/hsxxxx/.nvm/versions/node/v20.20.0//bin/node",
       autorestart: true,
       restart_delay: 2000,
       env_file: __dirname + "/apps/collector/.env",
-      env: {
-        NODE_ENV: "production",
-      },
+      env: { NODE_ENV: "production" },
     },
     {
       name: "bybit-builder",
       cwd: __dirname + "/apps/builder",
-      script: "node",
-      args: ["dist/index.js"],
+      script: "dist/index.js",
+      interpreter: "/home/hsxxxx/.nvm/versions/node/v20.20.0//bin/node",
       autorestart: true,
       restart_delay: 2000,
       env_file: __dirname + "/apps/builder/.env",
-      env: {
-        NODE_ENV: "production",
-      },
+      env: { NODE_ENV: "production" },
     },
   ],
 };
