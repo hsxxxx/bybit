80eecd270bb3d52693501f70483b4554b842f9f9
hsxxxx <hsxxxx@gmail.com>
2026-02-03 05:27:23 +0900

    pm2 config 파일 수정

--- apps/ecosystem.config.js
+++ apps/ecosystem.config.js
@@ -1,44 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "collector",
-      cwd: __dirname + "/collector",
+      name: "bybit-collector",
+      cwd: __dirname + "/apps/collector",
       script: "node",
       args: ["dist/index.js"],
       autorestart: true,
       restart_delay: 2000,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "builder",
-      cwd: __dirname + "/builder",
-      script: "node",
-      args: ["dist/index.js"],
-      autorestart: true,
-      restart_delay: 2000,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "sinker",
-      cwd: __dirname + "/sinker",
-      script: "node",
-      args: ["dist/index.js"],
-      autorestart: true,
-      restart_delay: 2000,
-      env: { NODE_ENV: "production" }
-    },
-    {
-      name: "viewer",
-      cwd: __dirname + "/viewer",
-      script: "node",
-      args: ["dist/index.js"],
-      autorestart: true,
-      restart_delay: 2000,
+      env_file: __dirname + "/apps/collector/.env",
       env: {
         NODE_ENV: "production",
-        PORT: "3100",
-        PORT_WS: "3101"
-      }
-    }
-  ]
+      },
+    },
+    {
+      name: "bybit-builder",
+      cwd: __dirname + "/apps/builder",
+      script: "node",
+      args: ["dist/index.js"],
+      autorestart: true,
+      restart_delay: 2000,
+      env_file: __dirname + "/apps/builder/.env",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
